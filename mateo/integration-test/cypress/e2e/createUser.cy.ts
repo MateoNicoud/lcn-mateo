3e99f48bec5f9f93e631d9480bc0d19f5982702c
MateoNicoud <mateo.nicoud@le-campus-numerique.fr>
2024-06-17 16:28:52 +0200

    1628 - Integration test

--- mateo/integration-test/cypress/e2e/createUser.cy.ts
+++ mateo/integration-test/cypress/e2e/createUser.cy.ts
@@ -10,107 +10,27 @@ describe("register", () => {
     cy.get('[placeholder="Email"]').type(name + "@example.com");
     cy.get('[placeholder="Password"]').type(name);
 
-    cy.intercept("POST", /users$/, {
-      statusCode: 200,
-      body: {
-        user: {
-          email: name + "@example.com",
-          token: "fake-jwt-token",
-          username: name,
-          bio: "",
-          image: ""
-        }
-      }
-    }).as("registerRequest");
-
     cy.get('[type="submit"]').click();
 
-    cy.wait("@registerRequest");
-    cy.url().should("match", /\/#\/$/);
-  });
+    cy.get(`[aria-label="${name}"]`).should("contain", name);
 
-  it("should display error message when submit the form that username already exist", () => {
-    cy.visit(ROUTES.REGISTER);
-
-    cy.get('[placeholder="Your Name"]').type(name);
-    cy.get('[placeholder="Email"]').type(name + "@example.com");
-    cy.get('[placeholder="Password"]').type(name);
-
-    cy.intercept("POST", /users$/, {
-      statusCode: 422,
-      body: {
-        errors: {
-          email: ["has already been taken"],
-          username: ["has already been taken"],
-        },
-      },
-    }).as("registerRequest");
-
-    cy.get('[type="submit"]').click();
-
-    cy.wait("@registerRequest");
-    cy.contains("email has already been taken");
-    cy.contains("username has already been taken");
-  });
-
-  it("should not allow visiting register page when the user is logged in", () => {
-    cy.intercept("POST", /users\/login$/, {
-      statusCode: 200,
-      body: {
-        user: {
-          email: name + "@example.com",
-          token: "fake-jwt-token",
-          username: name,
-          bio: "",
-          image: ""
-        }
-      }
-    }).as("loginRequest");
-
-    cy.visit(ROUTES.LOGIN);
-
-    cy.get('[type="email"]').type(name + "@example.com");
-    cy.get('[type="password"]').type(name);
-    cy.get('[type="submit"]').contains("Sign in").click();
-
-    cy.wait("@loginRequest");
-    cy.url().should("match", /#\/$/);
-
-    cy.visit(ROUTES.REGISTER);
-
-    cy.url().should("match", /\/#\/$/);
-  });
-});
-
-describe("no_article", () => {
-  it('should display "No articles are here... yet." when the user has no articles', () => {
-    cy.intercept("POST", /users\/login$/, {
-      statusCode: 200,
-      body: {
-        user: {
-          email: name + "@example.com",
-          token: "fake-jwt-token",
-          username: name,
-          bio: "",
-          image: ""
-        }
-      }
-    }).as("loginRequest");
-
-    cy.intercept("GET", `/articles?author=${name}`, {
-      articles: [],
-      articlesCount: 0,
-    }).as("getArticles");
-
-    cy.visit(ROUTES.LOGIN);
-
-    cy.get('[type="email"]').type(name + "@example.com");
-    cy.get('[type="password"]').type(name);
-    cy.get('[type="submit"]').contains("Sign in").click();
-
-    cy.wait("@loginRequest");
     cy.visit(`http://localhost:4137/#/profile/${name}`);
 
-    cy.contains("No articles are here... yet.");
+    cy.get(`[class="article-preview"]`).should(
+      "contain",
+      "No articles are here... yet.",
+    );
+    cy.visit("#/article/create");
+
+    cy.get('[placeholder="Article Title"]').type(name);
+    cy.get('[placeholder="What\'s this article about?"]').type(name);
+    cy.get('[placeholder="Write your article (in markdown)"]').type(name);
+    cy.get('[placeholder="Enter tags"]').type("éducation");
+    cy.get('[type="submit"]').contains("Publish Article").click();
+
+    cy.wait(1000);
+    cy.visit(`http://localhost:4137/#/profile/${name}`);
+
+    cy.get("a.preview-link h1").should("contain", name);
   });
 });
